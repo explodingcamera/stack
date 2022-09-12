54c13f99ff638093d22835700b512839ea3b0d15
Henry Gressmann <mail@henrygressmann.de>
2022-09-13 00:14:58 +0200

    use supervisord
    Signed-off-by: Henry Gressmann <mail@henrygressmann.de>

--- api/src/config.rs
+++ api/src/config.rs
@@ -2,9 +2,10 @@ use lazy_static::lazy_static;
 use ory_kratos_client::apis::configuration::Configuration;
 
 lazy_static! {
+    #[derive(Debug)]
     pub static ref KRATOS_CONFIG: Configuration = {
         Configuration {
-            base_path: "http://localhost:4433".to_string(),
+            base_path: std::env::var("KRATOS_URL").unwrap_or_else(|_| "http://localhost:4434".to_owned()),
             ..Default::default()
         }
     };
