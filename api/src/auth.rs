c12a3dd21a6be77642c5958a422cc41a585a0a51
Henry Gressmann <mail@henrygressmann.de>
2022-09-14 11:01:39 +0200

    feat: add optional user
    Signed-off-by: Henry Gressmann <mail@henrygressmann.de>

--- api/src/auth.rs
+++ api/src/auth.rs
@@ -7,6 +7,29 @@ use rocket::{
 use rocket_okapi::request::{OpenApiFromRequest, RequestHeaderInput};
 use thiserror::Error;
 
+fn get_auth_scheme(required: bool) -> RequestHeaderInput {
+    let mut requirements = schemars::Map::new();
+    if required {
+        requirements.insert("Session Token".to_owned(), vec![]);
+    };
+    RequestHeaderInput::Security(
+        "Session Token".to_owned(),
+        SecurityScheme {
+            description: Some("Authentication using ORY Kratos".to_string()),
+            data: SecuritySchemeData::ApiKey {
+                name: "Authorization".to_string(),
+                location: "header".to_string(),
+            },
+            extensions: Default::default(),
+        },
+        requirements,
+    )
+}
+
+pub struct OptionalUser {
+    pub session: Option<Session>,
+}
+
 pub struct User {
     pub session: Session,
 }
@@ -19,24 +42,52 @@ pub enum UserError {
     KratosError,
 }
 
+async fn request_to_session(request: &Request<'_>) -> Result<Session, UserError> {
+    let cookie = request
+        .cookies()
+        .get("Authorization")
+        .ok_or(UserError::NoSessionCookie)?;
+    let session_token = cookie.value();
+    let session = to_session(&crate::config::KRATOS_CONFIG, Some(session_token), None).await;
+    match session {
+        Ok(session) => Ok(session),
+        Err(_) => Err(UserError::KratosError),
+    }
+}
+
 #[rocket::async_trait]
 impl<'r> FromRequest<'r> for User {
     type Error = UserError;
 
     async fn from_request(req: &'r Request<'_>) -> request::Outcome<Self, Self::Error> {
-        let session_token = match req.headers().get_one("x-session-token") {
-            Some(header) => header,
-            None => return Outcome::Failure((Status::Unauthorized, UserError::NoSessionCookie)),
-        };
+        match request_to_session(req).await {
+            Ok(session) => Outcome::Success(User { session }),
+            Err(e) => Outcome::Failure((Status::Unauthorized, e)),
+        }
+    }
+}
 
-        let session =
-            match to_session(&crate::config::KRATOS_CONFIG, Some(session_token), None).await {
-                Ok(session) => session,
-                Err(_) => return Outcome::Failure((Status::Unauthorized, UserError::KratosError)),
-            };
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for OptionalUser {
+    type Error = UserError;
 
-        let user = User { session };
-        Outcome::Success(user)
+    async fn from_request(req: &'r Request<'_>) -> request::Outcome<Self, Self::Error> {
+        match request_to_session(req).await {
+            Ok(session) => Outcome::Success(OptionalUser {
+                session: Some(session),
+            }),
+            Err(_) => Outcome::Success(OptionalUser { session: None }),
+        }
+    }
+}
+
+impl<'r> OpenApiFromRequest<'r> for OptionalUser {
+    fn from_request_input(
+        _gen: &mut rocket_okapi::gen::OpenApiGenerator,
+        _name: String,
+        _required: bool,
+    ) -> rocket_okapi::Result<RequestHeaderInput> {
+        Ok(get_auth_scheme(false))
     }
 }
 
@@ -46,20 +97,6 @@ impl<'r> OpenApiFromRequest<'r> for User {
         _name: String,
         _required: bool,
     ) -> rocket_okapi::Result<RequestHeaderInput> {
-        let mut requirements = schemars::Map::new();
-        requirements.insert("Session Token".to_owned(), vec![]);
-
-        Ok(RequestHeaderInput::Security(
-            "Session Token".to_owned(),
-            SecurityScheme {
-                data: SecuritySchemeData::ApiKey {
-                    name: "x-session-token".to_owned(),
-                    location: "header".to_owned(),
-                },
-                description: Some("Used to authenticate as a user.".to_owned()),
-                extensions: schemars::Map::new(),
-            },
-            requirements,
-        ))
+        Ok(get_auth_scheme(true))
     }
 }
